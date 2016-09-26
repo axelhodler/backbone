266bd01d3cd21e215ee126919836c6571344c542
Axel Hodler <derxor@gmail.com>
2016-09-26 07:11:02 +0200

    add sample on how to check requests

--- src/me.test.js
+++ src/me.test.js
@@ -1,16 +1,36 @@
 'use strict';
 
-describe('sth', function() {
-  it('fails', function() {
+describe('MyModel', function() {
+  var server,
+    model;
+
+  beforeEach(function() {
     Backbone.$ = $;
+    server = sinon.fakeServer.create();
+    model = new window.MyModel({ id: 'axelhodler' });
+  });
+
+  it('uses the stubbed response', function() {
     var testData = { foo: 'bar' };
-    var server = sinon.fakeServer.create();
-    server.respondWith('GET', 'https://api.github.com/users/axelhodler', [200, {}, JSON.stringify(testData)]);
-    var model = new window.MyModel({ id: 'axelhodler' });
+    server.respondWith('GET', 'https://api.github.com/users/axelhodler', [200, { 'Content-Type': 'application/json' }, JSON.stringify(testData)]);
 
     model.fetch();
     server.respond();
 
     expect(model.get('foo')).to.equal('bar');
-  })
+  });
+
+  it('makes the correct requests', function() {
+    var xhr = sinon.useFakeXMLHttpRequest();
+    var requests = [];
+    xhr.onCreate = $.proxy(function(xhr) {
+      requests.push(xhr);
+    });
+
+    model.save();
+
+    var request = requests[0];
+    expect(request.method).to.equal('PUT');
+    expect(request.url).to.equal('https://api.github.com/users/axelhodler');
+  });  
 });
